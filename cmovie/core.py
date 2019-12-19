bfea99c1ac84d4ac9e97c71af264ea2564bb2b6f
Terry Turton <tlturton@lanl.gov>
2019-12-19 13:34:23 -0700

    move functionality to core

--- cmovie/core.py
+++ cmovie/core.py
@@ -1,3 +1,16 @@
+# Tools for cinema_movie application
 
+import yaml
+import sys
+
+################## Greet users #################################
 def hello():
-    print("hello")
+    sys.stdout.write ('{}\n'.format("welcome to cinema_movie") )
+
+################## Get parameters for movie ####################
+def get_params(file = 'movie.yaml'):
+    with open(file, 'r') as file:
+        try:
+            return yaml.full_load(file)
+        except yaml.YAMLError as exc:
+            sys.stderr.write ('YAML file error: {}\n'.format(exc))
